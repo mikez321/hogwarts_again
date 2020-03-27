46abae493d91fa3e71e0e360ee43f5727d2da222
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-27 09:37:41 -0600

    Create professor index test

---
+++ spec/features/professors_index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "when I visit /professors I see a list of professors" do
+  it "including their name age and specialty" do
+
+    professor_1 = Professor.create!(name: "Larry",
+                                    age: "75",
+                                    specialty: "Wood Carving")
+
+    professor_2 = Professor.create!(name: "Jim",
+                                    age: "40",
+                                    specialty: "Magic")
+
+   visit '/professors'
+
+    expect(page).to have_content("#{professor_1.name}")
+    expect(page).to have_content("#{professor_1.age}")
+    expect(page).to have_content("#{professor_1.specialty}")
+
+    expect(page).to have_content("#{professor_2.name}")
+    expect(page).to have_content("#{professor_2.age}")
+    expect(page).to have_content("#{professor_2.specialty}")
+  end
+end
